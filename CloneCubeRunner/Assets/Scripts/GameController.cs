83b5722cf380b28a78a689c74dbd058c2a4eb507
Phong.Nguyen <kubeo5239@gmail.com>
2024-06-30 19:42:38 +0700

    add UIManager ( xử lý panel gameover + btn relay )

--- CloneCubeRunner/Assets/Scripts/GameController.cs
+++ CloneCubeRunner/Assets/Scripts/GameController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;  //dùng để quản lý các thư mục Screnes
 public class GameController : MonoBehaviour
 {
     public GameObject obstacle;
@@ -13,10 +13,13 @@ public class GameController : MonoBehaviour
     bool m_isGameover;
     // Start is called before the first frame update
 
-
+    //xử lý view điểm 
+    UIManager m_ui; // thêm chữ tiền tố [m_] để phân biệt public / private / protected
     void Start()
     {
         m_spawTime = 0; // khởi tạo ban đầu
+        m_ui = FindAnyObjectByType<UIManager>();
+        m_ui.SetScoreText("Score :" + m_score);
     }
 
     // Update is called once per frame
@@ -26,6 +29,7 @@ public class GameController : MonoBehaviour
         if (m_isGameover)
         {
             m_spawTime = 0;
+            m_ui.ShowGameoverPanel(true);
             return; // return là dừng tắt cả các hoạt động phía dưới
         }
 
@@ -68,8 +72,18 @@ public class GameController : MonoBehaviour
 
     public void SetScore(int value) { m_score = value; }
     public int GetScore() { return m_score; }
-    public void ScoreIncrenment() { m_score++; }
+    public void ScoreIncrenment()
+    {
+        m_score++;
+        m_ui.SetScoreText("Score : " + m_score);
+    }
 
     public bool isGameover() { return m_isGameover; }
     public void SetGameover(bool state) { m_isGameover = state; }
+    //xử lý replay 
+    public void Relay()
+    {
+        // truyền tên file trong folder Scenes
+        SceneManager.LoadScene("Main");
+    }
 }
